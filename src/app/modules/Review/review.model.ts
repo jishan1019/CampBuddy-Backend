e8f0910411a28d33b636bc5927fb8ac07a9eb60b
jishan1019 <imransilamjishan80@gmail.com>
2024-12-06 21:31:42 +0600

    review model create

---
+++ src/app/modules/Review/review.model.ts
@@ -0,0 +1,32 @@
+import { model, Schema } from "mongoose";
+import { TReview } from "./review.interface";
+
+const reviewSchema = new Schema<TReview>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    order: {
+      type: Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const ReviewModel = model<TReview>("Review", reviewSchema);
